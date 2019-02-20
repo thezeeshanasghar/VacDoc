1cc52aa60a1cda226d10a10989242f11000dd04b
faisalmunir58 <faisalmunir58@yahoo.com>
2019-02-20 15:50:55 +0500

    brand inventory is show in html is Don

--- src/app/members/doctor/brand-inventory/brand-inventory.page.ts
+++ src/app/members/doctor/brand-inventory/brand-inventory.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { BrandService } from 'src/app/services/brand.service';
+import { ToastService } from 'src/app/shared/toast.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-brand-inventory',
@@ -7,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrandInventoryPage implements OnInit {
 
+  brandInventory:any
   constructor(
+    public loadingController: LoadingController,
+    private storage: Storage,
+    private brandService: BrandService,
+    private toastService: ToastService
   ) { }
 
   ngOnInit() {
+    this.storage.get(environment.DOCTOR_ID).then((val) => {
+      this.getBrandInventory(val);
+    })
   }
 
+  async getBrandInventory(id) {
+
+    const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+    await loading.present();
+    await this.brandService.getBrandInventory(id).subscribe(
+      res => {
+        if (res.IsSuccess) {
+          this.brandInventory = res.ResponseData;
+          // this.fg.controls['Amount'].setValue(this.brandInventory.Amount);
+          console.log(this.brandInventory);
+          loading.dismiss();
+        }
+        else {
+          loading.dismiss();
+          this.toastService.create(res.Message, 'danger');
+        }
+      },
+      err => {
+        loading.dismiss();
+        this.toastService.create(err, 'danger');
+      }
+    )
+  }
 }
