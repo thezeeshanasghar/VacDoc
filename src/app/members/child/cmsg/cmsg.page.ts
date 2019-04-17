a9a7be52e42cf0a33cab1187ca8953d0035e6770
faisalmunir58 <faisalmunir58@yahoo.com>
2019-04-17 10:33:22 +0500

    msg send on from patient page is Don

--- src/app/members/child/cmsg/cmsg.page.ts
+++ src/app/members/child/cmsg/cmsg.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { MessageService } from 'src/app/services/message.service';
+import { ToastService } from 'src/app/shared/toast.service';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cmsg',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CMsgPage implements OnInit {
 
-  constructor() { }
+  fg: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private loadingController: LoadingController,
+    private msgService: MessageService,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit() {
+    this.fg = this.formBuilder.group({
+      MobileNumber: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[0-9]{10}$')
+      ])),
+      'SMS': [null, Validators.required]
+    });
   }
+
+  async sendMsg() {
+    const loading = await this.loadingController.create({
+      message: 'loading'
+    });
+    await loading.present();
+    await this.msgService.sendMsg(this.fg.value)
+      .subscribe(res => {
+        if (res.IsSuccess) {
+          loading.dismiss();
+          this.router.navigate(['/members/child']);
+        }
+        else {
+          loading.dismiss();
+          this.toastService.create(res.Message, 'danger');
+        }
+      }, (err) => {
+        loading.dismiss();
+        this.toastService.create(err, 'danger');
+      });
+  }
+
+  validation_messages = {
+    'mobileNumber': [
+      { type: 'required', message: 'Mobile number is required like 3331231231' },
+      { type: 'pattern', message: 'Mobile number is required like 3331231231' }
+    ],
+    'Msg': [
+      { type: 'required', message: 'Msg is required.' }
+    ],
+  };
 
 }
