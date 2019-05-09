034033a419c82d733b544ce6fa33730e7d5aefcd
faisalmunir58 <faisalmunir58@yahoo.com>
2019-05-09 10:58:27 +0500

    signup service is Don

--- src/app/public/signup/step3/step3.page.ts
+++ src/app/public/signup/step3/step3.page.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-import { DoseService } from 'src/app/services/dose.service';
-import { ToastService } from 'src/app/shared/toast.service';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { DoseService } from "src/app/services/dose.service";
+import { ToastService } from "src/app/shared/toast.service";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
+import { SignupService } from "src/app/services/signup.service";
+import { Router } from "@angular/router";
 
 //https://www.joshmorony.com/dynamic-infinite-input-fields-in-an-ionic-application/
 
 @Component({
-  selector: 'app-step3',
-  templateUrl: './step3.page.html',
-  styleUrls: ['./step3.page.scss'],
+  selector: "app-step3",
+  templateUrl: "./step3.page.html",
+  styleUrls: ["./step3.page.scss"]
 })
 export class Step3Page implements OnInit {
-
   public fg: FormGroup;
 
   doses: any;
   constructor(
     private loadingController: LoadingController,
     private doseService: DoseService,
+    private signupService: SignupService,
     private toastService: ToastService,
-    private formBuilder: FormBuilder
-  ) { }
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.fg = this.formBuilder.group({});
@@ -31,35 +39,52 @@ export class Step3Page implements OnInit {
 
   async getVaccineInfo() {
     const loading = await this.loadingController.create({
-      message: 'Loading'
+      message: "Loading"
     });
     await loading.present();
     await this.doseService.getDoses().subscribe(
       res => {
         if (res.IsSuccess) {
           this.doses = res.ResponseData;
-          
-          this.doses.forEach(dose => {
-            let value = dose.MinGap==null ? '' : dose.MinGap;
-            this.fg.addControl(dose.Name, new FormControl(value, Validators.required));
-          });
-          
-          loading.dismiss();
-        }
-        else {
-          loading.dismiss();
-          this.toastService.create(res.Message, 'danger')
-        }
 
+          this.doses.forEach(dose => {
+            let value = dose.MinGap == null ? "" : dose.MinGap;
+            this.fg.addControl(
+              dose.Name,
+              new FormControl(value, Validators.required)
+            );
+          });
+
+          loading.dismiss();
+        } else {
+          loading.dismiss();
+          this.toastService.create(res.Message, "danger");
+        }
       },
       err => {
         loading.dismiss();
-        this.toastService.create(err, 'danger');
+        this.toastService.create(err, "danger");
       }
     );
   }
 
   onSubmit() {
-    console.log(this.fg.value);
+    this.addNewDoctor();
+  }
+
+  async addNewDoctor() {
+    await this.signupService.addDoctor().subscribe(
+      res => {
+        if (res.IsSuccess) {
+          this.toastService.create("successfully added");
+          this.router.navigate(["/login"]);
+        } else {
+          this.toastService.create(res.Message, "danger");
+        }
+      },
+      err => {
+        this.toastService.create(err, "danger");
+      }
+    );
   }
 }
--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -1,27 +1,54 @@
-import { Injectable } from '@angular/core';
-import { BaseService } from './base.service';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators'
+import { Injectable } from "@angular/core";
+import { BaseService } from "./base.service";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SignupService extends BaseService {
-
-  private readonly API_SCHEDULE = `${environment.BASE_URL}`
+  private readonly API_Doctor = `${environment.BASE_URL}`;
 
   personalData: any;
   clinicData: any;
   vaccineData: any;
-  constructor(
-    protected http: HttpClient
-  ) { super(http); }
+  constructor(protected http: HttpClient) {
+    super(http);
+  }
 
-  clientData() {
-    console.log(this.personalData);
-    console.log(this.clinicData);
-    console.log(this.vaccineData);
+  addDoctor(): Observable<any> {
+    let var1 = {
+      AdditionalInfo: this.personalData.AdditionalInfo,
+      ClinicDTO: {
+        Address: this.clinicData.Address,
+        ConsultationFee: this.clinicData.ConsultationFee,
+        EndTime: this.clinicData.EndTime,
+        Lat: this.clinicData.Lat,
+        Long: this.clinicData.Long,
+        Name: this.clinicData.Name,
+        OffDays: this.clinicData.OffDays,
+        PhoneNumber: this.clinicData.PhoneNumber,
+        StartTime: this.clinicData.StartTime
+      },
+      CountryCode: this.personalData.CountryCode,
+      DisplayName: this.personalData.DisplayName,
+      DoctorType: this.personalData.DoctorType,
+      Email: this.personalData.Email,
+      FirstName: this.personalData.FirstName,
+      LastName: this.personalData.LastName,
+      MobileNumber: this.personalData.MobileNumber,
+      PMDC: this.personalData.PMDC,
+      Password: this.personalData.Password,
+      PhoneNo: this.personalData.PhoneNo,
+      Qualification: this.personalData.Qualification,
+      ShowMobile: this.personalData.ShowMobile,
+      ShowPhone: this.personalData.ShowPhone
+    };
+    const url = `${this.API_Doctor}`;
+    return this.http
+      .post(url, var1, this.httpOptions)
+      .pipe(catchError(this.handleError));
   }
 }
