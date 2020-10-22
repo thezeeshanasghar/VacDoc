93a599f4b90bbf7af5e6b9d9c193d30122449b20
Irfan_Chaudhry <chirfan521@gmail.com>
2020-10-22 09:37:48 +0500

    message log added

--- src/app/members/message/message.page.ts
+++ src/app/members/message/message.page.ts
@@ -3,8 +3,9 @@ import { LoadingController } from '@ionic/angular';
 import { MessageService } from 'src/app/services/message.service';
 import { ToastService } from 'src/app/shared/toast.service';
 import { Storage } from '@ionic/storage';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { ClinicService } from "src/app/services/clinic.service";
+import { SMS } from '@ionic-native/sms/ngx';
 
 @Component({
   selector: 'app-message',
@@ -14,18 +15,21 @@ import { ClinicService } from "src/app/services/clinic.service";
 export class MessagePage implements OnInit {
 
   message: any;
+  Messages: any;
   constructor(
     public loadingController: LoadingController,
     private messageService: MessageService,
     private toastService: ToastService,
     private storage: Storage,
     public clinicService: ClinicService,
+    private sms: SMS
   ) { }
 
   ngOnInit() {
-    this.storage.get(environment.DOCTOR_Id).then((val) => {
-      this.getMsg(val);
-    });
+    // this.storage.get(environment.DOCTOR_Id).then((val) => {
+    //   this.getMsg(val);
+    // });
+    this.storage.get(environment.MESSAGES).then(messages=> {this.Messages = messages});
   }
 
   async getMsg(id) {
@@ -50,5 +54,17 @@ export class MessagePage implements OnInit {
         this.toastService.create(err, 'danger');
       }
     );
+  }
+  sendMessage(childMobile , message , created) {
+    this.sms.send(childMobile, message)
+          .then(()=>{
+            let obj = {'toNumber':childMobile , 'message': message , 'created': Date.now(), 'status':true};
+            this.Messages = this.Messages.filter(x=> x.created != created);
+            this.Messages.push(obj);
+            this.storage.set(environment.MESSAGES , this.Messages);
+          this.toastService.create("Message Sent Successful");
+          }).catch((error)=>{
+          this.toastService.create("Message Sent Failed" , "danger");
+          });
   }
 }
