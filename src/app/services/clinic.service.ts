026324cef3ccdb0248db84a266e4ad6047da01f0
mattaurrehman <atturehman@gmail.com>
2024-04-17 18:33:43 +0500

    remove the errros

--- src/app/services/clinic.service.ts
+++ src/app/services/clinic.service.ts
@@ -11,9 +11,6 @@ import { Storage } from "@ionic/storage";
   providedIn: "root"
 })
 export class ClinicService extends BaseService {
-  uploadMonogram(formData: FormData) {
-    throw new Error("Method not implemented.");
-  }
   clinics: any;
   doctorId: any;
   OnlineClinicId: any;
