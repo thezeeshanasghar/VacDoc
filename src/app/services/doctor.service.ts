81ae926d5c7d162cf9b8d0f6176eca3c9edf5772
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2024-04-30 12:27:47 +0500

    forgot service

--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -13,11 +13,18 @@ import { Storage } from "@ionic/storage";
 export class DoctorService extends BaseService {
   doctor: any;
   private readonly API_DOCTOR = `${environment.BASE_URL}doctor`
-
+  private readonly RESOURCE_URL = `${environment.RESOURCE_URL}`
   constructor(
     protected http: HttpClient, private storage: Storage
   ) {
     super(http);
+  }
+  forgotPassword(data): Observable<any> {
+    const url = `${this.RESOURCE_URL}forget`;
+    return this.http.post(url, data, this.httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   getDoctorProfile(docId: number): Observable<any> {
