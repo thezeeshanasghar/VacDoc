2ae136e6a2355b8c2c11f0e69542e548fdf2302d
Zeeshan Asghar <m.zeeshan.asghar@live.com>
2024-08-28 08:44:31 +0500

    Demo (#105)
    * creating a new branch
    
    * Test
    
    * changes on sign up page
    
    * Create ttt
    
    * remove test data
    
    * Changing in Brand Inventory and amount
    
    * remove s
    
    * changes brnd inv
    
    * Follow Up Future Date
    
    * Styling
    
    * invocie fixes
    
    * Skintechno to Vaccinationcenter in Whatsapp Alert
    
    * remove commented code
    
    * Revert "invocie fixes"
    
    This reverts commit 132d50c3a552ad5c9cbea1d4764e414da1fa3be2.
    
    * changes revert
    
    * update weblink of patient website
    
    * fix demo speel
    
    * Remove Validator From the Brand Inventory
    
    * Resolved conflict in environment.ts
    
    * Environment.ts file
    
    * change size in alert page
    
    * fixed whatsapp message
    
    ---------
    
    Co-authored-by: Muhammad Abubakar <Ahohihi721@gmail.com>
    Co-authored-by: Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>

--- src/app/members/alert/vaccine-alert/vaccine-alert.page.ts
+++ src/app/members/alert/vaccine-alert/vaccine-alert.page.ts
@@ -56,14 +56,14 @@ export class VaccineAlertPage implements OnInit {
 
 
   async ngOnInit() {
-   
-  
+
+
     this.getAlerts(this.selectedDate);
     await this.storage.get(environment.DOCTOR_Id).then(val => {
       this.doctorId = val;
     });
-    
-    
+
+
 
     await this.storage.get(environment.CLINIC_Id).then(clinicId => {
       this.clinicId = clinicId;
@@ -90,7 +90,7 @@ export class VaccineAlertPage implements OnInit {
   //           const displayName = this.displayName;
   //           this.displayName = doctorData.DisplayName;
 
-   
+
 
   //           console.log('Doctor Name:', doctorData.DisplayName);
   //           console.log('Doctor Email:', doctorData.Email);
@@ -100,7 +100,7 @@ export class VaccineAlertPage implements OnInit {
   //           console.log('Doctor Address:', doctorData.Clinics.Name);
 
 
-           
+
   //       }
   //     },
   //     err => {
@@ -116,7 +116,7 @@ export class VaccineAlertPage implements OnInit {
         if (res.IsSuccess) {
           const doctorData = res.ResponseData;
           console.log("Doctor Data is ", res.ResponseData);
-          
+
           // Doctor Information
           console.log('Doctor ID:', doctorData.Id);
           console.log('Doctor Name:', doctorData.DisplayName);
@@ -124,9 +124,9 @@ export class VaccineAlertPage implements OnInit {
           console.log('Doctor Phone:', doctorData.Phone);
           console.log('Doctor Specialization:', doctorData.Specialization);
           console.log('Doctor Address:', doctorData.Address);
-  
+
           this.displayName = doctorData.DisplayName;
-  
+
           // Clinic Information
           if (doctorData.Clinics && doctorData.Clinics.length > 0) {
             doctorData.Clinics.forEach((clinic, index) => {
@@ -136,17 +136,17 @@ export class VaccineAlertPage implements OnInit {
               console.log('Clinic Address:', clinic.Address);
               console.log('Clinic Phone:', clinic.PhoneNumber);
               console.log('Clinic Email:', clinic.Email);
-              
+
               if (clinic.Staff && clinic.Staff.length > 0) {
                 console.log(`Clinic ${index + 1} Staff:`);
                 clinic.Staff.forEach((staff, staffIndex) => {
                   console.log(`Staff Member ${staffIndex + 1}:`, staff.Name);
                 });
               }
-  
+
               console.log('-------------------'); // Separator between clinics
             });
-  
+
             // Store the first clinic's name (or adjust as needed)
             this.clinicName = doctorData.Clinics[0].Name;
             this.clinicPhoneNumber = doctorData.Clinics[0].PhoneNumber;
@@ -164,25 +164,25 @@ export class VaccineAlertPage implements OnInit {
     );
   }
   // Get childs get from server
-  async getChlid(numOfDays: number,formattedDate:string) {  
-      
+  async getChlid(numOfDays: number, formattedDate: string) {
+
     this.numOfDays = numOfDays;
     this.formattedDate = formattedDate;
     const loading = await this.loadingController.create({
       message: "Loading"
     });
     await loading.present();
-   
-    
+
+
     await this.alertService.getChild(this.formattedDate, this.numOfDays, this.clinicId).subscribe(
       res => {
-       
-        
+
+
         if (res.IsSuccess) {
           this.Childs = "";
           this.Childs = res.ResponseData;
-    
-          
+
+
 
           loading.dismiss();
         } else {
@@ -270,7 +270,7 @@ export class VaccineAlertPage implements OnInit {
 
   // send Alert Msg to childs
   async sendAlertMsg(id, childMobile, message) {
-   
+
     if (this.SMSKey == 0) {
       await this.alertService
         .sendIndividualAlertMsg(this.numOfDays, id)
@@ -344,12 +344,12 @@ export class VaccineAlertPage implements OnInit {
   callFunction(celnumber: string) {
     // Ensure the phone number is in the correct format
     const formattedNumber = celnumber.startsWith('0') ? celnumber : `0${celnumber}`;
-    
+
     this.callNumber.callNumber(formattedNumber, true)
       .then(res => console.log('Launched dialer!', res))
       .catch(err => console.log('Error launching dialer', err));
   }
-  
+
 
   // checkSmsPermission(): any {
   //   this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.SEND_SMS)
@@ -426,12 +426,12 @@ export class VaccineAlertPage implements OnInit {
   openWhatsApp(mobileNumber: string, childName: string, doseName: string) {
     // Ensure the patient's number starts with the country code
     const formattedPatientNumber = mobileNumber.startsWith('+92') ? mobileNumber : `+92${mobileNumber.replace(/^0/, '')}`;
-    
+
     // Format the clinic's phone number
     const formattedClinicNumber = this.clinicPhoneNumber.startsWith('+92') ? this.clinicPhoneNumber : `+92${this.clinicPhoneNumber.replace(/^0/, '')}`;
 
-    const message = encodeURIComponent(`Reminder: Vaccination ${doseName} of ${childName} is due. Please confirm your appointment.Thanks!\n${this.displayName}, ${this.clinicName}\nPhone Number ${formattedClinicNumber}\nhttps://vaccine.pk/\nhttps://vaccinationcenter.com/`);
-    
+    const message = encodeURIComponent(`Reminder: Vaccination ${doseName} of ${childName.trim()} is due. Please confirm your appointment. Thanks!\n${this.displayName}, ${this.clinicName}\nPhone Number ${formattedClinicNumber}\nLogin and check your record at https://vaccinationcentre.com`);
+
     let whatsappUrl: string;
 
     if (this.platform.is('android') || this.platform.is('ios')) {
@@ -459,7 +459,7 @@ export class VaccineAlertPage implements OnInit {
   getAlerts(date: string) {
     const formattedDate = this.formatDateToString(date);
 
-  this.getChlid(0, formattedDate);
+    this.getChlid(0, formattedDate);
   }
 
   async openDatePicker() {
