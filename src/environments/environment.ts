8d110f9930d7441f78c48a66096562fc17e91496
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2024-12-16 18:56:06 +0500

    revert env changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,41 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // BASE_URL: "https://stage.skintechno.com/api/",
+  // RESOURCE_URL: "https://stage.skintechno.com/",
+  
+
+  // for using web server
+  BASE_URL: "https://myapi.skintechno.com/api/",
+  RESOURCE_URL: "https://myapi.skintechno.com/",
+  
+  // // for local server
+  // BASE_URL: "https://localhost:5001/api/",
+  // RESOURCE_URL: "https://localhost:5001/",
+
+
+
+  USER: 'User',
+  DOCTOR_Id: "DoctorId",
+  CLINIC_Id: "ClinicId",
+  USER_Id: "UserId",
+  SMS: "SMS",
+  CLINICS: 'Clinics',
+  Childs: 'Childs',
+  CITY: 'City',
+  DOCTOR: 'Doctor',
+  ON_CLINIC: 'OnlineClinic',
+  MESSAGES: 'Messages'
+
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
