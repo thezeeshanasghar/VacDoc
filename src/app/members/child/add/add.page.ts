e3b98620d918da55b47ac563da278b9eb0babb1e
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2025-01-24 12:26:01 +0500

    Feature/additioninpdf (#156)
    * remove the years from the file Number2WeekPipe
    
    * fix the disable cnic input
    
    * fix the disable cnic input
    
    * fix version of type script
    
    * add changes
    
    * package update
    
    * changes in package.json
    
    * fixes that come in the build
    
    * romove the comment
    
    * changes in add page
    
    * remove check epi fucntion fix build error
    
    * fix epi done disable

--- src/app/members/child/add/add.page.ts
+++ src/app/members/child/add/add.page.ts
@@ -604,6 +604,12 @@ export class AddPage implements OnInit {
     console.log(days);
     this.epiDone = days > 272;
     this.isRadioDisabled = !this.epiDone; 
+    if (this.isRadioDisabled) {
+      
+      this.fg1.get('IsEPIDone').setValue(false);
+  }
+  
+
 }
 onTravelChange(event: any) {
     const selectedValue = event.detail.value; 
@@ -617,6 +623,9 @@ onTravelChange(event: any) {
     }
     this.fg1.get('CNIC').updateValueAndValidity(); 
     this.isRadioDisabled = this.isCnicRequired && !this.epiDone;
+
+    this.checkEpi()
+
 }
   calculateDiff(dateSent: string | number | Date) {
     let currentDate = new Date();
