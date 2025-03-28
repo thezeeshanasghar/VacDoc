8fd16afdc911d4da71c483f12ff2ae08c96defef
themuhammadattaurrehman <atta.randhawa86@gmail.com>
2025-03-28 15:39:09 +0500

    fixed

---
+++ src/app/shared/toast1.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  toast: HTMLIonToastElement;
+
+  constructor(public toastCtrl: ToastController) { }
+
+  async create(message: string, color = "success", ok = false, duration = 10000) {
+    if (this.toast) {
+      this.toast.dismiss();
+    }
+
+    this.toast = await this.toastCtrl.create({
+      message,
+      color: color,
+      duration: ok ? null : duration,
+      position: 'bottom',
+      // showCloseButton: ok,
+      // closeButtonText: 'OK'
+    });
+    this.toast.present();
+  }
+
+}
