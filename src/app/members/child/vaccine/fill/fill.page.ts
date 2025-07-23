4571f95c5034b68c239f6bfee2457b4e9fb6e76a
themuhammadattaurrehman <atta.randhawa86@gmail.com>
2025-07-23 14:38:11 +0500

    fix for tetnus infinite vaccine

--- src/app/members/child/vaccine/fill/fill.page.ts
+++ src/app/members/child/vaccine/fill/fill.page.ts
@@ -359,6 +359,12 @@ export class FillPage implements OnInit {
       if (myDate.getMonth() === 1 && myDate.getDate() === 29 && !this.isLeapYear(myDate.getFullYear())) {
         myDate.setDate(28);
       }
+    }else if (doseId === 122 && days === 3650) {
+      const currentYear = myDate.getFullYear();
+      myDate.setFullYear(currentYear + 10);
+      if (myDate.getMonth() === 1 && myDate.getDate() === 29 && !this.isLeapYear(myDate.getFullYear())) {
+        myDate.setDate(28);
+      }
     }else {
       myDate.setDate(myDate.getDate() + days);
     }
