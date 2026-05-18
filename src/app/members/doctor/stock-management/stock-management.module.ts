eaf58143386298294dc3168076e3db791538c5a1
salmanbajwa777-ops <salmanbajwa777@gmail.com>
2026-05-18 14:10:42 +0500

    fix(add.page): update IsPAApprove logic to handle optional user type feat(stock-management.module): add route for editing brand list

--- src/app/members/doctor/stock-management/stock-management.module.ts
+++ src/app/members/doctor/stock-management/stock-management.module.ts
@@ -11,10 +11,10 @@ const routes: Routes = [
     component: StockManagementPage
   },
   { path: 'add', loadChildren: () => import('./add/add.module').then(m => m.AddPageModule) },
+  { path: 'brandlist/edit/:brandId', loadChildren: () => import('./editbrandlist/edit.module').then(m => m.EditPageModule) },
   { path: 'brandlist/:brandId', loadChildren: () => import('./brandlist/brandlist.module').then(m => m.BrandListPageModule) },
   { path: 'adjust', loadChildren: () => import('./adjust/adjust.module').then(m => m.AdjustPageModule) },
   { path: 'stockinhand', loadChildren: () => import('./stockinhand/stockinhand.module').then(m => m.StockInHandPageModule) },
-  { path: 'brandlist/edit/:brandId', loadChildren: () => import('./editbrandlist/edit.module').then(m => m.EditPageModule) },
   { path: 'stock-transfer', loadChildren: () => import('./stock-transfer/stock-transfer.module').then(m => m.StockTransferPageModule) },
   { path: 'stock-transfer-history', loadChildren: () => import('./stock-transfer-history/stock-transfer-history.module').then(m => m.StockTransferHistoryPageModule) },
   { path: 'adjust-history', loadChildren: () => import('./adjust-history/adjust-history.module').then(m => m.AdjustHistoryPageModule) },
